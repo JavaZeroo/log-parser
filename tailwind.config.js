128866d5f5383c0e6efa6b66c51735b2dce46a9c
JavaZero <71128095+JavaZeroo@users.noreply.github.com>
2025-08-25 16:30:16 +0800

    fix: apply global dark form styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+import forms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forms],
 }
